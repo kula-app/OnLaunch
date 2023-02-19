1dcac91acd45e1e3cb9920f87ee0ed78572f6221
Florian Hochreiter <flo@kula.app>
2023-02-19 14:45:00 +0100

    feat: added user model to prisma schema (#68)

---
+++ prisma/migrations/20230219131042_added_user_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
