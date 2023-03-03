6047a663407c540f05af34e562aed99e01237ab7
Florian Hochreiter <flo@kula.app>
2023-03-03 19:07:27 +0100

    fix: fixed session provider in test (#31)

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -6,7 +6,7 @@ jest.mock("next/router", () => require("next-router-mock"));
 
 describe("Verify Page", () => {
   it("renders the verify page", () => {
-    render(<SessionProvider><Home /></SessionProvider>);
+    render(<Home />);
 
     // const heading = screen.getByRole("heading", {
     //   name: /Apps/i,
