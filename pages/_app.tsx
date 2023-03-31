c48ecab214f1fc6a07fec8ee4d1869c49aeaac31
Florian Hochreiter <flo@kula.app>
2023-03-31 13:33:47 +0200

    feat: todo resolved as it was discussed in meeting (#145)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 import Navbar from '../components/Navbar'
 
-// TODO: is this a component, a wrapper, a provider? Might be a good idea to move it to a non-page file (if possible).
-
 export default function App({ 
   Component, 
   pageProps: { session, ...pageProps },
