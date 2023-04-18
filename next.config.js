2ec6141bc2df2ee0cf6f6f2540e68af770a58673
Philip Niedertscheider <phil@kula.app>
2023-04-18 15:17:03 +0200

    fix: changed Next.js static generation timeout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 600,
 }
 
 module.exports = nextConfig
