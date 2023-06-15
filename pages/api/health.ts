86c00d5f7678fbbf2fe9522323bd0441a6341a7e
Philip Niedertscheider <phil@kula.app>
2023-06-15 14:48:48 +0200

    feat: added standardized health endpoint (#320)

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,7 +1,12 @@
+import { PrismaClient } from "@prisma/client";
 import { StatusCodes } from "http-status-codes";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as os from "os";
 import { loadConfig } from "../../config/loadConfig";
+import { Logger } from "../../util/logger";
+
+const logger = new Logger(__filename);
+const prisma = new PrismaClient();
 
 type Data = {
   status: string;
@@ -9,7 +14,7 @@ type Data = {
   uptime: number;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | {}>
 ) {
@@ -21,9 +26,106 @@ export default function handler(
       },
     });
   }
-  res.status(StatusCodes.OK).json({
-    status: "ok",
-    hostname: os.hostname(),
-    uptime: process.uptime(),
-  });
+  const result = await fetchHealthcheck();
+  if (result.status == "error") {
+    res
+      .status(StatusCodes.SERVICE_UNAVAILABLE)
+      .setHeader("Content-Type", "application/health+json")
+      .json(result);
+  } else {
+    res
+      .status(StatusCodes.OK)
+      .setHeader("Content-Type", "application/health+json")
+      .json(result);
+  }
+}
+
+interface HealthCheckResult {
+  /**
+   * Indicates whether the service status is acceptable or not.
+   *
+   *  - "ok": healthy
+   *  - "error": unhealthy
+   *  - "warn": healthy, with some concerns.
+   */
+  status: "ok" | "error" | "warn";
+  /**
+   * An object that provides detailed health statuses of additional downstream systems and endpoints which can affect the
+   * overall health of the main API.
+   */
+  checks: {
+    [keyof: string]: {
+      status: "ok" | "error" | "warn";
+      componentType?: string;
+      observedValue?: any;
+      observedUnit?: string;
+      time: string;
+    }[];
+  };
+}
+
+/**
+ * Response schema according as of this draft:
+ *
+ * https://github.com/inadarei/rfc-healthcheck/blob/master/draft-inadarei-api-health-check-06.txt
+ */
+async function fetchHealthcheck(): Promise<HealthCheckResult> {
+  const timestamp = new Date().toISOString();
+
+  // Create the base result
+  let checks: HealthCheckResult["checks"] = {
+    uptime: [
+      {
+        componentType: "system",
+        observedValue: os.uptime(),
+        observedUnit: "s",
+        status: "ok",
+        time: timestamp,
+      },
+    ],
+  };
+  try {
+    // Check the database connection
+    await prisma.$connect();
+    checks["postgres:connected"] = [
+      {
+        status: "ok",
+        componentType: "datastore",
+        time: timestamp,
+      },
+    ];
+  } catch (error) {
+    logger.error(`Failed to connect to database: ${error}`);
+    checks["postgres:connected"] = [
+      {
+        status: "error",
+        componentType: "datastore",
+        time: timestamp,
+      },
+    ];
+  }
+  return {
+    status: Object.values(checks)
+      .reduce((previousValue, checks) => previousValue.concat(checks), [])
+      .reduce<"ok" | "warn" | "error">((previousValue, check) => {
+        // If an error was already encountered, stick with it
+        if (previousValue === "error") {
+          return previousValue;
+        }
+        // If an error is found, return it
+        if (check.status === "error") {
+          return check.status;
+        }
+        // If a warning was already encountered, stick with it
+        if (previousValue === "warn") {
+          return previousValue;
+        }
+        // If a warning is found, return it
+        if (check.status === "warn") {
+          return check.status;
+        }
+        return "ok";
+      }, "ok"),
+    checks: checks,
+  };
 }
