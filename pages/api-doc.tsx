19fa7f561d7218f01e5d47fd9871713559e4ba33
Florian Hochreiter <flo@kula.app>
2024-05-21 18:13:22 +0200

    refactor: migration to redoc (#694)

--- pages/api-doc.tsx
+++ pages/api-doc.tsx
@@ -1,32 +1,12 @@
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { createSwaggerSpec } from "next-swagger-doc";
 import dynamic from "next/dynamic";
-import "swagger-ui-react/swagger-ui.css";
 
-const SwaggerUI = dynamic<{
-  spec: any;
-}>(import("swagger-ui-react"), { ssr: false });
+const RedocStandalone = dynamic(
+  () => import("redoc").then((mod) => mod.RedocStandalone),
+  { ssr: false }
+);
 
-function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />;
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const spec: Record<string, any> = createSwaggerSpec({
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "OnLaunch - Next Swagger API",
-        version: "1.0",
-      },
-    },
-  });
-
-  return {
-    props: {
-      spec,
-    },
-  };
+const ApiDoc = () => {
+  return <RedocStandalone specUrl="/api/openapi.yaml" />;
 };
 
 export default ApiDoc;
