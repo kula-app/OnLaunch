a8ccf0c326160a787c935383ec45664cbe743c66
Philip Niedertscheider <phil@kula.app>
2026-05-04 10:20:32 +0300

    chore: consolidate link action migrations into one

--- prisma/migrations/20250501134402_add_link_action/migration.sql
+++ prisma/migrations/20250501134402_add_link_action/migration.sql
@@ -1,5 +1,9 @@
+-- CreateEnum
+CREATE TYPE "MessageActionLinkTarget" AS ENUM ('in_app_browser', 'share_sheet', 'system_browser');
+
 -- AlterEnum
 ALTER TYPE "ActionType" ADD VALUE 'open_link';
 
 -- AlterTable
-ALTER TABLE "MessageAction" ADD COLUMN     "link" TEXT;
+ALTER TABLE "MessageAction" ADD COLUMN     "link" TEXT,
+ADD COLUMN     "linkTarget" "MessageActionLinkTarget";
--- prisma/migrations/20250501135959_add_target_to_link/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "MessageActionLinkTarget" AS ENUM ('in_app_browser', 'share_sheet', 'system_browser');
-
--- AlterTable
-ALTER TABLE "MessageAction" ADD COLUMN     "linkTarget" "MessageActionLinkTarget";
